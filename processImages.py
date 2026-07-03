434a9702fd6d77f1169e0630c2c415efb920ed70
Fraser Kent <fwpkent@gmail.com>
2026-07-02 22:45:15 -0700

    Continued Exploring Image Preprocessing

--- processImages.py
+++ processImages.py
@@ -2,8 +2,9 @@ import cv2
 import numpy
 import random
 
-for i in range(1,44):
-    currentPath = "ImageDataSet\\Gemini\\"+str(i)+".png"
+#for i in range(1,44):
+for i in range(1, 5):
+    currentPath = "TestingImageDataSet\\Gemini\\"+str(i)+".png"
     #print(currentPath)
     currentImage = cv2.imread(currentPath)
     resized = cv2.resize(currentImage, (224, 224))
@@ -11,21 +12,27 @@ for i in range(1,44):
 
     #noisyImage = None
     #if(random()%5 == 0):
-    guaNoise = numpy.random.normal(0, 20, resized.shape).astype(numpy.uint8)
+    guaNoise = numpy.random.normal(0, 15, resized.shape).astype(numpy.uint8)
     guaNoisyImage = cv2.add(resized, guaNoise)
 
     noisy_image = resized.copy()
     randNoise = numpy.random.randint(-25, 25 + 1, noisy_image.shape)
     randNoisyImage = numpy.clip(noisy_image + randNoise, 0, 255).astype(numpy.uint8)
+    blurredImage = cv2.blur(resized, (3,3));
+    #resizedGray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+    laplacianImage = cv2.Laplacian(resized, cv2.CV_16S, ksize=7)
     #elif(random()%5 == 0):
 
     resizedNormalized = resized.astype(float)/255.0
     guaNoisyNormalized = guaNoisyImage.astype(float)/255.0
     randNoisyNormalized = randNoisyImage.astype(float)/255.0
+    blurredNormalized = blurredImage.astype(float)/255.0
 
     cv2.imshow("Original Normalized",resizedNormalized)
     cv2.imshow("Noisy Normalized", guaNoisyNormalized)
     cv2.imshow("Rand Noise Normalized", randNoisyNormalized)
+    cv2.imshow("Blurred" , blurredNormalized)
+    cv2.imshow("Laplacian: ", laplacianImage)
     cv2.waitKey(1000)
     cv2.destroyAllWindows()
 
