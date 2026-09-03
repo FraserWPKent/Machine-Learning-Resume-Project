976d42347c1eb5723573706a25439ee8d7509be7
Fraser Kent <fwpkent@gmail.com>
2026-09-03 00:29:46 -0700

    Updating the saved models to start over with my new dataset

--- src/AiImageDetectionProject/main.py
+++ src/AiImageDetectionProject/main.py
@@ -16,14 +16,14 @@ def main():
 
     if(int(sys.argv[2])):
         # Initializing the Build Datasets when working on kaggle
-        trainingDataset = db.AiImageDetectorDataset("/kaggle/input/datasets/williamkent1234321/10000-real-vs-fake-faces-stylegan-modified/Training", training=True)
-        testingDataset = db.AiImageDetectorDataset("/kaggle/input/datasets/williamkent1234321/10000-real-vs-fake-faces-stylegan-modified/Testing", training=False)
+        trainingDataset = db.AiImageDetectorDataset("/kaggle/input/datasets/williamkent1234321/updated-ai-image-detected-dataset/Training", training=True)
+        testingDataset = db.AiImageDetectorDataset("/kaggle/input/datasets/williamkent1234321/updated-ai-image-detected-dataset/Validate", training=False)
         trainingLoader = DataLoader(trainingDataset, shuffle=True, batch_size=128, num_workers=4, pin_memory=True)
         testingLoader = DataLoader(testingDataset, shuffle=False, batch_size=128, num_workers=4, pin_memory=True)
     else:
         #Initializing the Build Datasets when working on my personal machine
         trainingDataset = db.AiImageDetectorDataset("ImageDataset/Training", training=True)
-        testingDataset = db.AiImageDetectorDataset("ImageDataset/Testing", training=False)
+        testingDataset = db.AiImageDetectorDataset("ImageDataset/Validate", training=False)
         trainingLoader = DataLoader(trainingDataset, shuffle=True, batch_size=128, num_workers=8, pin_memory=True)
         testingLoader = DataLoader(testingDataset, shuffle=False, batch_size=128, num_workers=8, pin_memory=True)
     
